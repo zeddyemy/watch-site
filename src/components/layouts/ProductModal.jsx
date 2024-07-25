13b42b955738f391b682f892d7341eb571375fb5
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2024-07-25 12:57:47 +0100

    fix placement

---
+++ src/components/layouts/ProductModal.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductModal = () => {
+    return (
+        <>
+            <div id="product-modal" className="">
+                <div id="product-modal-card" className="card">
+                    
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProductModal
